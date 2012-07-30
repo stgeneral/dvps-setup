138e325ec430a19b145422535aa522d1b511bece
Dmitry Babenko <dmitry@babenko.in.ua>
2012-07-30 17:50:29 +0300

    Fix path to templates

--- dvps-setup.sh
+++ dvps-setup.sh
@@ -1,15 +1,15 @@
-echo "Updating the system..."
+echo "Updating the system"
 sudo aptitude update
 sudo aptitude upgrade -y
 
-echo "Installing VirtualBox guest utils..."
+echo "Installing VirtualBox guest utils"
 sudo aptitude install -y virtualbox-ose-guest-utils
 
-echo "Adding 'dev' and 'www-data' users to 'vboxsf' group..."
+echo "Adding 'dev' and 'www-data' users to 'vboxsf' group"
 sudo usermod -aG vboxsf www-data
 sudo usermod -aG vboxsf dev
 
-echo "Creating webserver directory structure..."
+echo "Creating webserver directory structure"
 cd /media/sf_webserver
 mkdir -p backups public_html logs sites
 cd ~
@@ -18,17 +18,17 @@ ln -s /media/sf_webserver/public_html
 ln -s /media/sf_webserver/logs
 ln -s /media/sf_webserver/sites
 
-echo "Configuring Apache..."
-sudo cp -f ~/dev/dvps-setup/templates/httpd.conf /etc/apache2/
-sudo cp -f ~/dev/dvps-setup/templates/dvps.dev /etc/apache2/sites-available/
+echo "Configuring Apache"
+sudo cp -f ~/dvps-setup/templates/httpd.conf /etc/apache2/
+sudo cp -f ~/dvps-setup/templates/dvps.dev /etc/apache2/sites-available/
 sudo a2ensite dvps.dev
 sudo a2enmod rewrite
 
-echo "Installing development software..."
+echo "Installing development software"
 # sudo aptitude install -y php-codesniffer php5-sqlite php5-xdebug git-core
 sudo aptitude install -y phpmyadmin php5-curl php5-gd php5-mcrypt php-apc mc subversion git-core
 
-echo "Restarting Apache..."
+echo "Restarting Apache"
 sudo service apache2 restart
 
 #
